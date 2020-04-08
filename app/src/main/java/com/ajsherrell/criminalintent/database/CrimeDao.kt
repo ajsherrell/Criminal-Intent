2f934ca299f5ad70add755485d56e9fdee2c09b2
ajsherrell <ajsherrell.ajacks@gmail.com>
2020-04-08 15:45:34 -0400

    add fake database file into emulator device file explorer

--- app/src/main/java/com/ajsherrell/criminalintent/database/CrimeDao.kt
+++ app/src/main/java/com/ajsherrell/criminalintent/database/CrimeDao.kt
@@ -1,5 +1,6 @@
 package com.ajsherrell.criminalintent.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
 import com.ajsherrell.criminalintent.Crime
@@ -9,9 +10,9 @@ import java.util.*
 interface CrimeDao {
 
     @Query("SELECT * FROM crime")
-    fun getCrimes(): List<Crime>
+    fun getCrimes(): LiveData<List<Crime>>
 
     @Query("SELECT * FROM crime WHERE id=(:id)")
-    fun getCrime(id: UUID): Crime?
+    fun getCrime(id: UUID): LiveData<Crime?>
 
 }
